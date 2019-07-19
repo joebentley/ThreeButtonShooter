082f94a6a1940fef8b4d2e3dd73b32ab0a8aad97
Joe Bentley <jbentley@star.sr.bham.ac.uk>
2019-07-19 10:13:39 +0100

    Show final score on final screen

--- Scripts/GameOverScreen.gd
+++ Scripts/GameOverScreen.gd
@@ -2,6 +2,9 @@ extends ColorRect
 
 signal new_game
 
+func _ready():
+	$Score.text = "Final score: " + str(Globals.score)
+
 func _input(event):
 	if event.is_action_pressed("key_c"):
 		emit_signal("new_game")
